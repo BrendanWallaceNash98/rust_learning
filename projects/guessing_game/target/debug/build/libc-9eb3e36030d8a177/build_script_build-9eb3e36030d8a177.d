32d7dece6ac029e45937328767adb964fc856aa9
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2025-07-23 19:21:47 +1000

    Remove directory_name from tracking

--- projects/guessing_game/target/debug/build/libc-9eb3e36030d8a177/build_script_build-9eb3e36030d8a177.d
+++
@@ -1,5 +0,0 @@
-/Users/brendanwallace-nash/learning_rust/projects/guessing_game/target/debug/build/libc-9eb3e36030d8a177/build_script_build-9eb3e36030d8a177.d: /Users/brendanwallace-nash/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs
-
-/Users/brendanwallace-nash/learning_rust/projects/guessing_game/target/debug/build/libc-9eb3e36030d8a177/build_script_build-9eb3e36030d8a177: /Users/brendanwallace-nash/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs
-
-/Users/brendanwallace-nash/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.174/build.rs:
--- projects/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/brendanwallace-nash/learning_rust/projects/guessing_game/target/debug/guessing_game: /Users/brendanwallace-nash/learning_rust/projects/guessing_game/src/main.rs
